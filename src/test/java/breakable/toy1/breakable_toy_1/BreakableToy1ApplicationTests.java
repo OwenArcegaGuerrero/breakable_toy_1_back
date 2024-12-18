8cac163f907400f28567a21049887506eb02ae4e
= <=>
2024-12-18 17:00:11 -0600

    Initial Project Structure

---
+++ src/test/java/breakable/toy1/breakable_toy_1/BreakableToy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package breakable.toy1.breakable_toy_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakableToy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
