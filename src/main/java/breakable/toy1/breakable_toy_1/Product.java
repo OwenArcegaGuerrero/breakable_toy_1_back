2a22511259d1988d29a0d1ce5d2ef4b2e3f9c0e1
= <=>
2024-12-19 22:08:49 -0600

    Implemented Product class and tests

---
+++ src/main/java/breakable/toy1/breakable_toy_1/Product.java
@@ -0,0 +1,77 @@
+package breakable.toy1.breakable_toy_1;
+
+import java.time.LocalDate;
+
+public class Product {
+    private Long id = 1l;
+    private String name;
+    private String category;
+    private Double unitPrice;
+    private LocalDate expirationDate;
+    private Long quantityInStock;
+    private LocalDate creationDate;
+    private LocalDate updateDate;
+
+    Product(String name, String category, Double unitPrice, LocalDate expirationDate, Long quantityInStock, LocalDate creationDate, LocalDate updateDate) throws Exception{
+        this.id = assignId();
+    
+        if(name == null || name.length() <= 0 || name.length() > 120){
+            throw new Exception("Invalid name");
+        } else {
+            this.name = name;
+        }
+
+        if(category == null || category.length() <= 0){
+            throw new Exception("Invalid category");
+        } else {
+            this.category = category;
+        }
+
+        if(unitPrice == null || unitPrice <= 0.0){
+            throw new Exception("Invalid Unit Price");
+        } else {
+            this.unitPrice = unitPrice;
+        }
+
+        this.expirationDate = expirationDate;
+
+        if(quantityInStock == null || quantityInStock <= 0l) {
+            throw new Exception("Invalid Quantity In Stock");
+        } else {
+            this.quantityInStock = quantityInStock;
+        }
+        
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+    }
+
+    private long assignId(){ return id++; }
+
+    public long getId(){ return this.id; }
+
+    public String getName(){ return this.name; }
+
+    public void setName(String newName){ this.name = newName; }
+
+    public String getCategory(){ return this.category; }
+
+    public void setCategory(String newCategory){ this.category = newCategory; }
+
+    public Double getUnitPrice(){ return this.unitPrice; }
+
+    public void setUnitPrice(Double newUnitPrice){ this.unitPrice = newUnitPrice; }
+
+    public LocalDate getExpirationDate(){ return this.expirationDate; }
+
+    public void setExpirationDate(LocalDate newExpirationDate){ this.expirationDate = newExpirationDate; }
+
+    public long getQuantityInStock(){ return this.quantityInStock; }
+
+    public void setQuantityInStock(long newQuantityInStock){ this.quantityInStock = newQuantityInStock; }
+
+    public LocalDate getCreationDate(){ return this.creationDate; }
+
+    public LocalDate getUpdateDate(){ return this.updateDate; }
+
+    public void setUpdateDate(LocalDate newUpdateDate){ this.updateDate = newUpdateDate; }
+}
--- src/test/java/breakable/toy1/breakable_toy_1/BreakableToy1ApplicationTests.java
+++ src/test/java/breakable/toy1/breakable_toy_1/BreakableToy1ApplicationTests.java
@@ -1,5 +1,8 @@
 package breakable.toy1.breakable_toy_1;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,50 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BreakableToy1ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void ShouldCreateAProductLocally() throws Exception {
+		Product product = new Product("Tomato", "Fruit", 10.0, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		assertThat(product).isNotNull();
+
+		String longName = "a".repeat(121);
+		Exception nameTooLong = assertThrows(Exception.class, () -> {
+			new Product(longName, "Fruit", 10.0, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		});
+		assertThat("Invalid name").isEqualTo(nameTooLong.getMessage());
+
+		Exception shortName = assertThrows(Exception.class, () -> {
+			new Product("", "Fruit", 10.0, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		});
+		assertThat("Invalid name").isEqualTo(shortName.getMessage());
+
+		Exception nullName = assertThrows(Exception.class, () -> {
+			new Product(null, "Fruit", 10.0, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		});
+		assertThat("Invalid name").isEqualTo(nullName.getMessage());
+
+		Exception shortCategory = assertThrows(Exception.class, () -> {
+			new Product("Tomato", "", 10.0, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		});
+		assertThat("Invalid category").isEqualTo(shortCategory.getMessage());
+
+		Exception nullCategory = assertThrows(Exception.class, () -> {
+			new Product("Tomato", null, 10.0, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		});
+		assertThat("Invalid category").isEqualTo(nullCategory.getMessage());
+
+		Exception zeroUnitPrice = assertThrows(Exception.class, () -> {
+			new Product("Tomato", "Fruit", 0.0, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		});
+		assertThat("Invalid Unit Price").isEqualTo(zeroUnitPrice.getMessage());
+
+		Exception negativeUnitPrice = assertThrows(Exception.class, () -> {
+			new Product("Tomato", "Fruit", -10.0, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		});
+		assertThat("Invalid Unit Price").isEqualTo(negativeUnitPrice.getMessage());
+
+		Exception nullUnitPrice = assertThrows(Exception.class, () -> {
+			new Product("Tomato", "Fruit", null, LocalDate.now(), 10l, LocalDate.now(), LocalDate.now());
+		});
+		assertThat("Invalid Unit Price").isEqualTo(nullUnitPrice.getMessage());
 	}
 
 }
