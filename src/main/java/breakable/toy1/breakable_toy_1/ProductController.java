8f769549b26f75ee8dfb03f4e18e272697cb09cc
= <=>
2024-12-31 17:55:28 -0600

    allowed-localhost

--- src/main/java/breakable/toy1/breakable_toy_1/ProductController.java
+++ src/main/java/breakable/toy1/breakable_toy_1/ProductController.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "http://localhost:8080")
 public class ProductController {
 
     @Autowired
